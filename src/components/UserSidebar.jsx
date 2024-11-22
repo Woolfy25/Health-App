e99dce8ba01e6816acfd9d144dd63be3db8db06a
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-11-22 23:23:41 +0200

    added images

--- src/components/UserSidebar.jsx
+++ src/components/UserSidebar.jsx
@@ -18,7 +18,7 @@ const UserSidebar = () => {
   }, [dispatch]);
 
   return (
-    <section className="w-2/5 flex flex-col pt-12 gap-24 bg-neutral-100 pl-8 tablet:w-full tablet:h-full tablet:pt-16 tablet:pb-16 tablet:pl-6 tablet:pr-6 phone:w-full phone:h-full phone:pt-16 phone:pb-16 phone:pl-3 phone:pr-3">
+    <section className="bg-image-home w-2/5 flex flex-col pt-12 gap-24 bg-neutral-100 pl-8 tablet:w-full tablet:h-full tablet:pt-16 tablet:pb-16 tablet:pl-6 tablet:pr-6 phone:w-full phone:h-full phone:pt-16 phone:pb-16 phone:pl-3 phone:pr-3">
       <div className="flex justify-end items-center gap-3 mt-8 pr-6 tablet:hidden phone:hidden">
         <p className="text-sm font-bold text-slate-500">{name}</p>
         <div className="bg-slate-300 h-8 w-[2px] tablet:hidden phone:hidden"></div>
