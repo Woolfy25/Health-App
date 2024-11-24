b45804768106487f0de1321e0660ee6504d385c4
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-11-25 00:17:20 +0200

    ssl

--- src/REDUX/auth/operations.js
+++ src/REDUX/auth/operations.js
@@ -3,8 +3,10 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { selectToken } from "../auth/selectors";
 
-const API_URL = "http://13.48.177.15:3000/health/";
-axios.defaults.baseURL = API_URL || "http://localhost:3000/health/";
+// const API_URL = "http://13.48.177.15:3000/health/";
+const API_URL_SSL = "https://13.48.177.15:3000/health/";
+
+axios.defaults.baseURL = API_URL_SSL || "http://localhost:3000/health/";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
